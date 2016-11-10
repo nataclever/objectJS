bb84681104e3d685ae5a61856cf2b466508e6d7e
Nataliia <natakozova@gmail.com>
2016-11-10 13:17:06 +0000

    initial

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,17 @@
-google.addEventListener('click', function(e) {
+/*google.addEventListener('click', function(e) {
     e.preventDefault();
     alert('Its a joke!');
-});
+});*/
+
+
+
+
+var n = 20;
+var fibonacci = [1, 1];
+
+for ( i = 1; i < n; i++) 
+{ 
+    fibonacci[1] = fibonacci [i - 1] + fibonacci[i -2]; 
+    
+}
+    console.log(fibonacci(0,n))
